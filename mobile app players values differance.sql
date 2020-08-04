d3aabee2f130269dca2ed4ec277a1a8bc9b460ef
hossam eldien mokhtar <69170538+hossameldien1984@users.noreply.github.com>
2020-08-04 12:36:11 +0200

    Update mobile app players values differance.sql

--- mobile app players values differance.sql
+++ mobile app players values differance.sql
@@ -1,11 +1,4 @@
-/*
-Please review the dataset schema provided on the 'data_2' table. 
-Please create SQL query presenting difference between the last value over time and 
-the first value over time for each player_id (clarification: not the biggest and
- the lowest values difference).
-Desired final schema is: player_id, difference
-*/
-use sales;
+
 SELECT 
     summery.player_id, (lv.value - fv.value) difference_val
 FROM
